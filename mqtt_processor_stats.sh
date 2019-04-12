4d3d96c9bbf14ecc7312c86cadabe359b9fb46c0
DataQub3 <7069058+DataQub3@users.noreply.github.com>
2019-04-12 16:15:10 +0100

    fixed bug where get longer and longer topic names by removing any journal entries containing average in them

--- mqtt_processor_stats.sh
+++ mqtt_processor_stats.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/journalctl -u mqtt_logger --since="`/bin/date -d "-5 min" +"%Y-%m-%d %H:%M:%S"`" | grep -v "^--" | cut -d ":" -f 4 | perl -nle 's/^\s//g; s/\s/, /g; print $_' | /usr/bin/python3 /home/pi/IoTitan/mqtt_processor_stats.py
+/bin/journalctl -u mqtt_logger --since="`/bin/date -d "-5 min" +"%Y-%m-%d %H:%M:%S"`" | grep -v "^--" | grep -v "average" | cut -d ":" -f 4 | perl -nle 's/^\s//g; s/\s/, /g; print $_' | /usr/bin/python3 /home/pi/IoTitan/mqtt_processor_stats.py
